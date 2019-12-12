b9c3c8c2a1bca3087e81b238d91ae80a1f4f80e9
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-12-12 11:37:53 +0200

    Update main.go

--- code/main.go
+++ code/main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 	a := App{}
-	a.Initialize("root", "password", "mysql:3306", "rest_api_example")
+	a.Initialize("root", "password", "mysql", "rest_api_example")
 	a.Run(":8080")
 }
